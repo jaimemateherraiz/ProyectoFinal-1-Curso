e7d09d00d2a7dd216373a51641cea01552525dd3
jaimemateherraiz <150348896+jaimemateherraiz@users.noreply.github.com>
2024-05-19 11:34:47 +0200

    creando marketplace.html

--- swap-and-grow/app/app.py
+++ swap-and-grow/app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request, redirect, url_for
 from conexion_db import DatabaseConnection
 
 app = Flask(__name__)
@@ -7,20 +7,27 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-
-@app.route('/marketplace')
+@app.route('/marketplace', methods=['GET', 'POST'])
 def marketplace():
+    if request.method == 'POST':
+        # Procesar datos del formulario y agregarlos a la base de datos
+        nombre = request.form['nombre']
+        descripcion = request.form['descripcion']
+        precio = request.form['precio']
+        # Aquí iría el código para agregar los datos a la base de datos
+
+    # Obtener productos de la base de datos
     try:
         with DatabaseConnection() as db:
-            productos = db.execute_query("SELECT id, nombre, descripcion, precio FROM productos")
+            productos = db.execute_query("SELECT identificador, nombre, descripcion, precio FROM productos")
             if productos is None:
                 productos = []
             print("Productos obtenidos de la base de datos:", productos)
     except Exception as e:
         print(f"Error al acceder a la base de datos: {e}")
         productos = []
-    return render_template('marketplace.html', productos=productos)
 
+    return render_template('marketplace.html', productos=productos)
 
 @app.route('/about')
 def about():
